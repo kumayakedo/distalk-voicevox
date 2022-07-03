f7a31d1baef581d0c731bc86173c2d97bb858988
kumayakedo <108029353+kumayakedo@users.noreply.github.com>
2022-07-03 22:36:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ import emoji
 import json
 import psycopg2
 
-prefix = os.getenv('DISCORD_BOT_PREFIX', default='🦑')
+prefix = os.getenv('DISCORD_BOT_PREFIX', default='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 voicevox_key = os.environ['VOICEVOX_KEY']
 voicevox_speaker = os.getenv('VOICEVOX_SPEAKER', default='2')
@@ -16,6 +16,7 @@ client = commands.Bot(command_prefix=prefix)
 with open('emoji_ja.json', encoding='utf-8') as file:
     emoji_dataset = json.load(file)
 database_url = os.environ.get('DATABASE_URL')
+text_channel_id = None
 
 @client.event
 async def on_ready():
@@ -34,6 +35,7 @@ async def on_guild_remove(guild):
 
 @client.command()
 async def 接続(ctx):
+    global text_channel_id
     if ctx.message.guild:
         if ctx.author.voice is None:
             await ctx.send('ボイスチャンネルに接続してから呼び出してください。')
@@ -45,8 +47,10 @@ async def 接続(ctx):
                     await ctx.voice_client.disconnect()
                     await asyncio.sleep(0.5)
                     await ctx.author.voice.channel.connect()
+                    text_channel_id = ctx.channel.id
             else:
                 await ctx.author.voice.channel.connect()
+                text_channel_id = ctx.channel.id
 
 @client.command()
 async def 切断(ctx):
@@ -108,92 +112,95 @@ async def 辞書確認(ctx):
 
 @client.event
 async def on_message(message):
+    global text_channel_id
     if message.guild.voice_client:
-        if not message.author.bot:
-            if not message.content.startswith(prefix):
-                text = message.content
+        if message.channel.id == text_channel_id:
+            if not message.author.bot:
+                if not message.content.startswith(prefix):
+                    text = message.content
 
-                # Add author's name
-                text = message.author.name + '、' + text
+                    # Add author's name
+                    text = message.author.name + '、' + text
 
-                # Replace dictionary
-                with psycopg2.connect(database_url) as conn:
-                    with conn.cursor() as cur:
-                        sql = 'SELECT * FROM dictionary WHERE guildId = %s'
-                        value = (message.guild.id, )
-                        cur.execute(sql, value)
-                        rows = cur.fetchall()
-                        for row in rows:
-                            word = row[1]
-                            kana = row[2]
-                            text = text.replace(word, kana)
+                    # Replace dictionary
+                    with psycopg2.connect(database_url) as conn:
+                        with conn.cursor() as cur:
+                            sql = 'SELECT * FROM dictionary WHERE guildId = %s'
+                            value = (message.guild.id, )
+                            cur.execute(sql, value)
+                            rows = cur.fetchall()
+                            for row in rows:
+                                word = row[1]
+                                kana = row[2]
+                                text = text.replace(word, kana)
 
-                # Replace new line
-                text = text.replace('\n', '、')
+                    # Replace new line
+                    text = text.replace('\n', '、')
 
-                # Replace mention to user
-                pattern = r'<@!?(\d+)>'
-                match = re.findall(pattern, text)
-                for user_id in match:
-                    user = await client.fetch_user(user_id)
-                    user_name = f'、{user.name}へのメンション、'
-                    text = re.sub(rf'<@!?{user_id}>', user_name, text)
+                    # Replace mention to user
+                    pattern = r'<@!?(\d+)>'
+                    match = re.findall(pattern, text)
+                    for user_id in match:
+                        user = await client.fetch_user(user_id)
+                        user_name = f'、{user.name}へのメンション、'
+                        text = re.sub(rf'<@!?{user_id}>', user_name, text)
 
-                # Replace mention to role
-                pattern = r'<@&(\d+)>'
-                match = re.findall(pattern, text)
-                for role_id in match:
-                    role = message.guild.get_role(int(role_id))
-                    role_name = f'、{role.name}へのメンション、'
-                    text = re.sub(f'<@&{role_id}>', role_name, text)
+                    # Replace mention to role
+                    pattern = r'<@&(\d+)>'
+                    match = re.findall(pattern, text)
+                    for role_id in match:
+                        role = message.guild.get_role(int(role_id))
+                        role_name = f'、{role.name}へのメンション、'
+                        text = re.sub(f'<@&{role_id}>', role_name, text)
 
-                # Replace Unicode emoji
-                text = re.sub(r'[\U0000FE00-\U0000FE0F]', '', text)
-                text = re.sub(r'[\U0001F3FB-\U0001F3FF]', '', text)
-                for char in text:
-                    if char in emoji.UNICODE_EMOJI['en'] and char in emoji_dataset:
-                        text = text.replace(char, emoji_dataset[char]['short_name'])
+                    # Replace Unicode emoji
+                    text = re.sub(r'[\U0000FE00-\U0000FE0F]', '', text)
+                    text = re.sub(r'[\U0001F3FB-\U0001F3FF]', '', text)
+                    for char in text:
+                        if char in emoji.UNICODE_EMOJI['en'] and char in emoji_dataset:
+                            text = text.replace(char, emoji_dataset[char]['short_name'])
 
-                # Replace Discord emoji
-                pattern = r'<:([a-zA-Z0-9_]+):\d+>'
-                match = re.findall(pattern, text)
-                for emoji_name in match:
-                    emoji_read_name = emoji_name.replace('_', ' ')
-                    text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
+                    # Replace Discord emoji
+                    pattern = r'<:([a-zA-Z0-9_]+):\d+>'
+                    match = re.findall(pattern, text)
+                    for emoji_name in match:
+                        emoji_read_name = emoji_name.replace('_', ' ')
+                        text = re.sub(rf'<:{emoji_name}:\d+>', f'、{emoji_read_name}、', text)
 
-                # Replace URL
-                pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
-                text = re.sub(pattern, '、画像', text)
-                pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
-                text = re.sub(pattern, '、URL', text)
+                    # Replace URL
+                    pattern = r'https://tenor.com/view/[\w/:%#\$&\?\(\)~\.=\+\-]+'
+                    text = re.sub(pattern, '画像', text)
+                    pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+(\.jpg|\.jpeg|\.gif|\.png|\.bmp)'
+                    text = re.sub(pattern, '、画像', text)
+                    pattern = r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+'
+                    text = re.sub(pattern, '、URL', text)
 
-                # Replace spoiler
-                pattern = r'\|{2}.+?\|{2}'
-                text = re.sub(pattern, '伏せ字', text)
+                    # Replace spoiler
+                    pattern = r'\|{2}.+?\|{2}'
+                    text = re.sub(pattern, '伏せ字', text)
 
-                # Replace laughing expression
-                if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
-                    while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
-                        text = text[:-1]
-                    text = text[:-1] + '、ワラ'
+                    # Replace laughing expression
+                    if text[-1:] == 'w' or text[-1:] == 'W' or text[-1:] == 'ｗ' or text[-1:] == 'W':
+                        while text[-2:-1] == 'w' or text[-2:-1] == 'W' or text[-2:-1] == 'ｗ' or text[-2:-1] == 'W':
+                            text = text[:-1]
+                        text = text[:-1] + '、ワラ'
 
-                # Add attachment presence
-                for attachment in message.attachments:
-                    if attachment.filename.endswith((".jpg", ".jpeg", ".gif", ".png", ".bmp")):
-                        text += '、画像'
-                    else:
-                        text += '、添付ファイル'
+                    # Add attachment presence
+                    for attachment in message.attachments:
+                        if attachment.filename.endswith((".jpg", ".jpeg", ".gif", ".png", ".bmp")):
+                            text += '、画像'
+                        else:
+                            text += '、添付ファイル'
 
-                mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale=1'
-                while message.guild.voice_client.is_playing():
-                    await asyncio.sleep(0.5)
-                source = await discord.FFmpegOpusAudio.from_probe(mp3url)
-                message.guild.voice_client.play(source)
+                    mp3url = f'https://api.su-shiki.com/v2/voicevox/audio/?text={text}&key={voicevox_key}&speaker={voicevox_speaker}&intonationScale=1'
+                    while message.guild.voice_client.is_playing():
+                        await asyncio.sleep(0.5)
+                    source = await discord.FFmpegOpusAudio.from_probe(mp3url)
+                    message.guild.voice_client.play(source)
     await client.process_commands(message)
 
-'''@client.event
+'''
+@client.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None:
         if member.id == client.user.id:
@@ -235,7 +242,8 @@ async def on_voice_state_update(member, before, after):
                     await asyncio.sleep(0.5)
                     await member.guild.voice_client.disconnect()
                     await asyncio.sleep(0.5)
-                    await after.channel.connect()'''
+                    await after.channel.connect()
+'''
 
 @client.event
 async def on_command_error(ctx, error):
